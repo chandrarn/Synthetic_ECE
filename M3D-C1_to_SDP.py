e57d6e27f12fccbd941b155a6f28677eb7bca66c
rianc <rianc@eofe10.mit.edu>
2025-03-11 15:31:04 -0400

    Synch

--- M3D-C1_to_SDP.py
+++ M3D-C1_to_SDP.py
@@ -26,17 +26,17 @@ def save_C1_SDP_format(filename='/nobackup1/wenhaw42/Linear/01_n1_test_cases/100
     B0 = np.sqrt( B0[0]**2 + B0[1]**2 + B0[2]**2 )
     # Save in SDP format
     ds = xr.Dataset(data_vars = dict(\
-             rr=(['r'],R), zz=(['z'],Z), ne=(['r','z'],ne.data), \
-             te=(['r','z'],te.data), bb=(['r','z'], B1), ) )
+             rr=(['r'],R), zz=(['z'],Z), ne=(['r','z'],ne.data.T), \
+             te=(['r','z'],te.data.T), bb=(['r','z'], B1.T), ) )
     ds0 = xr.Dataset(data_vars = dict(\
-             rr=(['r'],R), zz=(['z'],Z), ne=(['r','z'],ne0.data), \
-             te=(['r','z'],te0.data), bb=(['r','z'], B0), ) )
+             rr=(['r'],R), zz=(['z'],Z), ne=(['r','z'],ne0.data.T), \
+             te=(['r','z'],te0.data.T), bb=(['r','z'], B0.T), ) )
     
     ds0.to_netcdf('C1.h5_equ_ufile.cdf')
     ds.to_netcdf('C1.h5_{0:0>4}_ufile.cdf'.format(1))
 
 ######################################################################3
-def get_fields_from_C1(filename,n,phi=0,slice=0):
+def get_fields_from_C1(filename,n,phi=0,slice=0,debug=True):
     # Slice acts as timepoint
     # Get psi grid, B grid, R,Z coords
     print('Reading: %s'%filename)
@@ -49,22 +49,26 @@ def get_fields_from_C1(filename,n,phi=0,slice=0):
     b_field = C1py.read_field('bfield', slice=slice, filename=filename, points=200,
                         rrange=None, zrange=None,phi=phi, iequil=None,idiff=False)
     b_field0 = C1py.read_field('bfield', slice=slice, filename=filename, points=200,
-                        rrange=None, zrange=None,phi=phi, iequil=True,idiff=False)
+                        rrange=None, zrange=None,phi=phi, iequil=None,idiff=True)
     # print(b_field.data.shape,b_field.coords['R'].values.shape)
     # raise SyntaxError
 
 
     ne = C1py.read_field('ne', slice=slice, filename=filename, points=200,
-                        rrange=None, zrange=None,phi=phi, iequil=None,idiff=False)
+                        rrange=None, zrange=None,phi=phi+1, iequil=None,idiff=False)
     ne0 = C1py.read_field('ne', slice=slice, filename=filename, points=200,
-                        rrange=None, zrange=None,phi=phi, iequil=True,idiff=False)
+                        rrange=None, zrange=None,phi=phi, iequil=None,idiff=False)
     
     te = C1py.read_field('te', slice=slice, filename=filename, points=200,
-                        rrange=None, zrange=None,phi=phi, iequil=None,idiff=False)
+                        rrange=None, zrange=None,phi=phi+1, iequil=None,idiff=False)
     te0 = C1py.read_field('te', slice=slice, filename=filename, points=200,
-                        rrange=None, zrange=None,phi=phi, iequil=True,idiff=False)
+                        rrange=None, zrange=None,phi=phi, iequil=None,idiff=False)
  
     R=b_field.coords['R'].values;Z=b_field.coords['Z']
+    
+    if debug:
+        b_field.to_netcdf('b_field.cdf')
+        b_field0.to_netcdf('b_field_0.cdf')
 
     # C1 file data
     isrc = fio_py.open_source(fio_py.FIO_M3DC1_SOURCE,filename)
